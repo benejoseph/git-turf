7d1d664aaf5cefcf37f123312a6e2009e5186e5d
Benjamin Joseph <benejoseph@gmail.com>
2013-08-03 22:24:27 -0700

    Added git parsing routines

---
+++ gitparse.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+import re
+
+def parserecord(log):
+    regexhash = 'commit[\s]+[a-f0-9]{20,}[^\n]'
+    regexauthor = 'Author.*[^\n]'
+    regexdate = 'Date.*[^\n]'
+
+    temp = re.search(regexhash,log).group()
+    temp = temp.split(' ')
+    myhash = temp[1]
+
+    temp = re.search(regexauthor,log).group()
+    temp = temp.split(': ')[1]
+    myauthor = temp.split('<')[0]
+
+    temp = re.search(regexdate,log).group()
+    temp = temp.split(': ')[1].strip()
+    mydate = temp
+
+    temp = re.search(mydate + '[\s\S]*',log).group()
+    comments = temp.split(mydate)[1]
+
+    lines = comments.split('\n')
+    mycomment = ''
+    mymods = []
+    isfirst = True
+    for line in lines:
+        if line is not '':
+            if isfirst:
+                mycomment = line.strip()
+                isfirst = False
+            else:
+                mymods.append(line)         
+            
+
+    data = {'hash' : myhash, 'author' : myauthor, 'date' : mydate, 'comment' : mycomment, 'mods' : mymods}
+
+    return data
+
+
+def parsegitlog(log):
+    regexheader = 'commit[\s]+[a-f0-9]{20,}\nAuthor.*\nDate.*\n'
+
+    iter = re.finditer(regexheader,txt)
+    indices = [m.start(0) for m in iter]
+    indices.append(len(txt))
+
+    records = []
+    for j in range(len(indices)-1):
+        records.append(txt[indices[j]:indices[j+1]])
+
+    data = []
+    for record in records:
+        data.append(parserecord(record))
+
+    return data
+
+
+
+
